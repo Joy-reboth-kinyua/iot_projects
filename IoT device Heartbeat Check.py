f57c7d7bc1794137b3231249f1cfc515f156f9e0
Joy Reboth <135380604+Joy-reboth-kinyua@users.noreply.github.com>
2025-02-24 10:20:24 +0300

    initial commit

--- IoT device Heartbeat Check.py
+++ IoT device Heartbeat Check.py
@@ -2,5 +2,9 @@ def device_offline(timestamps, T):
     for i in range(1, len(timestamps)):  #start from the second timestamp
         if timestamps[i] - timestamps[i-1] > T:
             return True #device went offline
-        return False #device remained online
+    return False #device remained online
+
+timestamps = [0,3,6,15,18]
+T = 5
+print(device_offline(timestamps, T))
 
